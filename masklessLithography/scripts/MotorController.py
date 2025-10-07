6844f94ec3e1d1438f77f3705f805a2ea78ceae4
RylanBosquez <rylanbosquez@icloud.com>
2025-10-06 20:05:30 -0500

    modified MotorController class

--- masklessLithography/scripts/MotorController.py
+++ masklessLithography/scripts/MotorController.py
@@ -1,9 +1,11 @@
 from machine import Pin, PWM
 import utime
 
+
 class MotorController:
     
-    def __init__(self, stepGPIO, directionGPIO, ms1GPIO=None, ms2GPIO=None, ms3GPIO=None):
+    
+    def __init__(self, stepGPIO=0, directionGPIO=1, ms1GPIO=None, ms2GPIO=None, ms3GPIO=None):
         
         self.directionPin = Pin(directionGPIO, Pin.OUT)
         self.stepPin = Pin(stepGPIO, Pin.OUT)
@@ -11,7 +13,8 @@ class MotorController:
         self.ms2Pin = Pin(ms2GPIO, Pin.OUT) if ms2GPIO is not None else None
         self.ms3Pin = Pin(ms3GPIO, Pin.OUT) if ms3GPIO is not None else None
         
-    def rotateMotor(self, steps=200, forwards=True, pulseMicroSeconds=1000):
+        
+    def rotateMotor(self, steps=200, forwards=True, pulseMicroSeconds=400):
         
         self.directionPin.value(1 if forwards else 0)
         
@@ -22,6 +25,7 @@ class MotorController:
             
             self.stepPin.value(0)
             utime.sleep_us(pulseMicroSeconds)
+          
             
     def setMicrostepping(self, mode):
         
@@ -48,5 +52,10 @@ class MotorController:
                 
 if __name__ == '__main__':
     
-    motor = MotorController(0, 1)
-    motor.rotateMotor(steps=200, forwards=True)
+    motor1 = MotorController(stepGPIO=0, directionGPIO=1)
+    motor2 = MotorController(stepGPIO=2, directionGPIO=3)
+    motor3 = MotorController(stepGPIO=4, directionGPIO=5)
+    
+    motor1.rotateMotor(steps=200, forwards=False)
+    motor2.rotateMotor(steps=200, forwards=False)
+    motor3.rotateMotor(steps=200, forwards=False)
