7abfa44e37fb82524c474bcc55a7de30a089d7f3
Moses Ongole <mosesdongole@gmail.com>
2026-01-22 14:25:35 -0600

    changes

--- masklessLithographyGUI/camera.py
+++ masklessLithographyGUI/camera.py
@@ -1,59 +1,76 @@
 # Used for testing live footage from the basler camera
 # for the maskless photolithography machine.
-# The footage needs to be in the form of a PyQt5 widget.
-# Otherwise, this can be done with a simple OpenCV window.
-# Updated 1/20/2025 by S. Jacob Finch
-# 
-# Thanks to Google and 'eyllanesc' on stack overflow
+# Updated for PyQt6
 
-from PyQt5.QtWidgets import  QWidget, QLabel, QApplication
-from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt6.QtWidgets import QWidget, QLabel, QApplication
+from PyQt6.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
+from PyQt6.QtGui import QImage, QPixmap
+
 import sys
 import cv2
 
-class Thread(QThread):
+
+class CameraThread(QThread):
     updatePixmap = pyqtSignal(QImage)
 
     def run(self):
-        # Try to open the basler camera
         try:
-            # Index will vary based on system.
-            # For a laptop, the built-in webcam is usually 0.
-            # Sometimes it takes a few seconds to appear.
-            cap = cv2.VideoCapture(0) 
+            cap = cv2.VideoCapture(0)
         except Exception as e:
             print(f"Exception: {e}")
             print("No camera found.")
             return
-        # Continuously get frames from the camera
+
         while True:
             ret, frame = cap.read()
-            if ret:
-                # https://stackoverflow.com/a/55468544/6622587
-                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                h, w, ch = rgbImage.shape
-                bytesPerLine = ch * w
-                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
-                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-                self.updatePixmap.emit(p)
+            if not ret:
+                continue
+
+            # Convert BGR → RGB
+            rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            h, w, ch = rgbImage.shape
+            bytesPerLine = ch * w
+
+            qtImage = QImage(
+                rgbImage.data,
+                w,
+                h,
+                bytesPerLine,
+                QImage.Format.Format_RGB888
+            )
+
+            scaled = qtImage.scaled(
+                640,
+                480,
+                Qt.AspectRatioMode.KeepAspectRatio,
+                Qt.TransformationMode.SmoothTransformation
+            )
+
+            self.updatePixmap.emit(scaled)
+
 
 class CameraFeed(QWidget):
     def __init__(self):
         super().__init__()
-        # Make a label to hold the camera feed
+
+        self.setWindowTitle("Camera Feed")
+
         self.label = QLabel(self)
         self.label.resize(640, 480)
-        # Use threading to continuously update the camera
-        th = Thread(self)
-        th.updatePixmap.connect(self.setImage)
-        th.start()
-        self.show()
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.thread = CameraThread(self)
+        self.thread.updatePixmap.connect(self.setImage)
+        self.thread.start()
+
     @pyqtSlot(QImage)
-    def setImage(self, image):
+    def setImage(self, image: QImage):
         self.label.setPixmap(QPixmap.fromImage(image))
 
-# app = QApplication(sys.argv)
-# camFeed = CameraFeed()
-# camFeed.show()
-# app.exec()
+
+# 🔎 Standalone test
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     camFeed = CameraFeed()
+#     camFeed.show()
+#     sys.exit(app.exec())
