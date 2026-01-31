cc114a92a53351a5cd737d790d0f44ad833dda83
S. Jacob Finch <jacobfinch2005@gmail.com>
2026-01-31 21:38:51 +0000

    PyPylon works on RPi now, camera is in GUI.

--- masklessLithographyGUI - Copy/camera.py
+++ masklessLithographyGUI - Copy/camera.py
@@ -1,13 +1,21 @@
-# Used for testing live footage from the basler camera
-# for the maskless photolithography machine.
-# The footage needs to be in the form of a PyQt6 widget.
-# Updated 1/26/2025 by S. Jacob Finch
+# Used to see live footage from the basler camera of the maskless photolithography machine.
+# The footage needs to be retrieved using PyPylon and returned in the form of a PyQt6 widget.
+# Updated 1/31/2025 by S. Jacob Finch
 
 from PyQt6.QtWidgets import  QWidget, QLabel, QApplication
 from PyQt6.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
 from PyQt6.QtGui import QImage, QPixmap
 from pypylon import pylon
-import cv2
+import cv2, sys
+import numpy as np
+
+converter = pylon.ImageFormatConverter()
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+    
+tl_factory = pylon.TlFactory.GetInstance()
+camera = pylon.InstantCamera()
+camera.Attach(tl_factory.CreateFirstDevice())
 
 class Thread(QThread):
     updatePixmap = pyqtSignal(QImage)
@@ -15,25 +23,33 @@ class Thread(QThread):
     def run(self):
         # Try to open the basler camera
         try:
-            # Index will vary based on system.
-            # For a laptop, the built-in webcam is usually 0.
-            # Sometimes it takes a few seconds to appear.
-            cap = cv2.VideoCapture(camera) 
+            camera.Open()
+            # The camera will continuously grab frames one by one
+            camera.StartGrabbing(pylon.GrabStrategy_OneByOne)
         except Exception as e:
-            print(f"Exception: {e}")
-            print("No camera found.")
+            print(f"Could not open camera. Exception: {e}")
             return
-        # Continuously get frames from the camera
-        while True:
-            ret, frame = cap.read()
-            if ret:
-                # https://stackoverflow.com/a/55468544/6622587
-                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                h, w, ch = rgbImage.shape
-                bytesPerLine = ch * w
-                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format.Format_RGB888)
-                p = convertToQtFormat.scaled(640, 480, Qt.AspectRatioMode.KeepAspectRatio)
+        # Constantly be updating the Pixmap with the new frame
+        while camera.IsGrabbing():
+            # Get the frame
+            grab = camera.RetrieveResult(2000, pylon.TimeoutHandling_ThrowException)
+            if grab.GrabSucceeded():
+                # Convert to BRG888 pixel format (see top of this script)
+                image = converter.Convert(grab)
+                arr = image.GetArray()
+                arr = np.ascontiguousarray(arr) # silly little numpy formatting
+                h, w, _ = arr.shape
+                # Convert to a QImage so we can use it with the PyQt6 GUI
+                qimg = QImage(arr.data, w, h, arr.strides[0], QImage.Format.Format_BGR888)
+                qimg = qimg.copy() # Separate from numpy array in memory, for some reason this helped
+                # Scale it to a good size
+                p = qimg.scaled(640, 480, Qt.AspectRatioMode.KeepAspectRatio)
+                # If all goes well, update the CameraFeed by emitting a signal
                 self.updatePixmap.emit(p)
+            
+            # We are done with this frame, so release it from memory so that the memory can be reused
+            grab.Release()
+                
 
 class CameraFeed(QWidget):
     def __init__(self):
@@ -43,13 +59,16 @@ class CameraFeed(QWidget):
         self.label.resize(640, 480)
         # Use threading to continuously update the camera
         th = Thread(self)
-        th.updatePixmap.connect(self.setImage)
+        # When a pyqtSignal is emitted, run the "self.setImage" function
+        # This will update the camera box in the GUI every time the signal is received
+        th.updatePixmap.connect(self.setImage) 
         th.start()
         self.show()
-    @pyqtSlot(QImage)
+    @pyqtSlot(QImage) # Listen for QImage signals
     def setImage(self, image):
         self.label.setPixmap(QPixmap.fromImage(image))
 
+# FOR DEBUGGING: Uncomment to make a window appear when running this script
 # app = QApplication(sys.argv)
 # camFeed = CameraFeed()
 # camFeed.show()
--- masklessLithographyGUI - Copy/pypylon_test.py
+++ masklessLithographyGUI - Copy/pypylon_test.py
@@ -1,22 +1,21 @@
 # Opens a window with the Basler ACE USB camera feed.
 # From this tutorial: https://pythonforthelab.com/blog/getting-started-with-basler-cameras/
-
-import numpy as npfrom
+# https://www.iditect.com/faq/python/convert-python-opencv-image-numpy-array-to-pyqt-qpixmap-image.html
+import numpy as np
+from PIL import Image
 import sys
 from PyQt6.QtWidgets import  QWidget, QLabel, QApplication
 from PyQt6.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
 from PyQt6.QtGui import QImage, QPixmap
 from pypylon import pylon
 
-def np_arr_to_qimage(arr):
-    try:
-        h, w = arr.shape
-        bytesPerLine = 3 * w
-        convertToQtFormat = QImage(arr.data, w, h, bytesPerLine, QImage.Format.Format_RGB888)
-        qimg = convertToQtFormat.scaled(640, 480, Qt.AspectRatioMode.KeepAspectRatio)
-        return qimg
-    except Exception as e:
-        print(e)
+app = QApplication(sys.argv)
+
+# def np_arr_to_qimage(arr):
+
+converter = pylon.ImageFormatConverter()
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
     
 tl_factory = pylon.TlFactory.GetInstance()
 camera = pylon.InstantCamera()
@@ -25,15 +24,27 @@ camera.Open()
 camera.StartGrabbing(1)
 grab = camera.RetrieveResult(2000, pylon.TimeoutHandling_ThrowException)
 if grab.GrabSucceeded():
-    arr = grab.GetArray()
+    image = converter.Convert(grab)
+    arr = image.GetArray()
+    arr = np.ascontiguousarray(arr)
     print("Grabbed an image")
-    qimg = np_arr_to_qimage(arr)
-    label = QLabel()
-    label.setPixmap(QPixmap.fromImage(qimg))
+    print(arr)
+    h, w, _ = arr.shape
+    qimg = QImage(arr.data, w, h, arr.strides[0], QImage.Format.Format_BGR888)
+    qimg = qimg.copy()
     print("converted to QImage")
+    # qimg = qimg.scaled(640, 480, Qt.AspectRatioMode.KeepAspectRatio)
+    print("scaled")
+    qimg.save("qimg_test.png")
+    print("qimg saved")
+    print(qimg)
     
+grab.Release()
+ 
 camera.Close()
 
-app = QApplication(sys.argv)
-# label.show()
+label = QLabel()
+pixmap = QPixmap.fromImage(qimg)
+label.setPixmap(pixmap)
+label.show()
 app.exec()
